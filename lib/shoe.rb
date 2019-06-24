1b2a3e1e43b908ba4b91866c069c58d8e226ac89
Jamie Vullo <jvullo@protonmail.com>
2019-06-24 13:11:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,15 @@
 class Shoe
   
+  BRANDS = []
+  
+  def initialize(brands)
+    @brands = brands
+  end
+  
+  def brands=(brands)
+    @brands = brands
+    BRANDS << brands
+  end
+    
+  
 end
